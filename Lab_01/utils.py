e00dcf17275a14d19cd509a6deafde8621a4af4d
S1gge <sigge.parakhnevich@gmail.com>
2025-03-27 22:24:30 +0100

    working with class and voting_cls

--- Lab_01/utils.py
+++ Lab_01/utils.py
@@ -8,50 +8,49 @@ import seaborn as sns
 
 # plt.pie cardio, cholesterol, smoke, height
 def eda_pieplot(df):
-    fig,axs = plt.subplots(2, 2, figsize = (12,14))
+    fig,axes = plt.subplots(2, 2, figsize = (12,14))
+    axes = axes.flatten()
 
-    axs[0,0].pie(df['cardio'].value_counts(), autopct='%1.2f%%',  explode=[0.1,0], shadow=True)
-    axs[0,0].set_title('Proportion of people who tests positive or negative for \n cardiovascualr disease.')
-    axs[0,0].legend(loc='best', labels=['Negative', 'Positive'])
+    titles = ['Proportion of people who tests positive or negative for \n cardiovascualr disease.',
+              'Proportion of people who have normal, above normal and well \n above normal cholesterol levels.',
+              'Proportion of people who smoke or not.',
+              'Proportion of women and men who have cardiovascular disease.']
+    labels = [['Negative', 'Positive'],
+              ['Normal','Above normal', 'Well above normal'],
+              ["Don't smoke","Smoke"],
+              ['Male','Female']]
+    features =['cardio', 'cholesterol', 'smoke', 'cardio'==1]
+    for i, (title, label, feature) in enumerate(zip(titles, labels, features)):
 
-    axs[0,1].pie(df['cholesterol'].value_counts(),  autopct='%1.2f%%', explode=[0.04,0.1,0.1], shadow=True)
-    axs[0,1].set_title('Proportion of people who have normal, above normal and well \n above normal cholesterol levels.')
-    axs[0,1].legend(loc='best', labels=['Normal','Above normal', 'Well above normal'])
+        plot = axes[i].pie(df['cardio'].value_counts(), autopct='%1.2f%%',  explode=[0.1,0], shadow=True)
+        axes[i].set_title(title)
+        axes[i].legend(loc='best', labels=label)
 
-    axs[1,0].pie(df['smoke'].value_counts(), autopct='%1.2f%%', explode=[0.2,0], shadow=True)
-    axs[1,0].set_title('Proportion of people who smoke or not.')
-    axs[1,0].legend(loc='best', labels=["Don't smoke","Smoke"])
-
-    axs[1,1].pie(df[df['cardio']==1]['gender'].value_counts(), autopct='%1.2f%%', explode=[0.1,0], shadow=True)
-    axs[1,1].set_title('Proportion of women and men who have cardiovascular disease.')
-    axs[1,1].legend(loc='best', labels=['Male','Female'])
-
-    return
-
+    return plot
+ 
+ 
 # histplot age, weight, height
 def eda_histplot(df):
-    fig,axs = plt.subplots(3,1, figsize = (10,10))
+    fig,axes = plt.subplots(3,1, figsize = (10,10))
+    axes = axes.flatten()
 
-    sns.histplot(data=[a/365 for a in df['age']], bins=30, ax=axs[0])
-    axs[0].set_title('Age distribution.')
-    axs[0].set_xlabel('Age, years.')
+    titles = ['Age distribution.', 'Weight destripution.', 'Height distribution.']
+    labels = ['Age, years.', 'Weight, kg', 'Height, cm.']
+    
+    for i, (title, label) in enumerate(zip(titles, labels)):
 
-    sns.histplot(data=df['weight'], bins=30, ax=axs[1])
-    axs[1].set_title('Weight destripution.')
-    axs[1].set_xlabel('Weight, kg')
-
-    sns.histplot(data=df['height'], bins=30, ax=axs[2])
-    axs[2].set_title('Height distribution.')
-    axs[2].set_xlabel('Height, cm.')
+        plot=sns.histplot(data=[a/365 for a in df['age']], bins=30, ax=axes[i])
+        axes[i].set_title(title)
+        axes[i].set_xlabel(label)
 
     plt.tight_layout()
-
-    return
+    plt.show()
+    return plot
 
 
 # Visualiseringar andel sjukdomar
 def plot_disease(df):
-    fig, axes = plt.subplots(3, 2, dpi=100, figsize=(12, 14))
+    fig, axes = plt.subplots(3, 2, dpi=200, figsize=(12, 14))
     axes = axes.flatten()
 
     df_h=df.copy()
@@ -72,13 +71,13 @@ def plot_disease(df):
     plt.show()
     return plot
 
+
 # Heatmap av korrelatione
 def plot_heat(df):
     df = pd.get_dummies(df, drop_first=True)
     plt.figure(figsize=(10,8))
     plot = sns.heatmap(df.corr(), annot=True, annot_kws={'size':8}, fmt= '.2f', cmap='coolwarm', linewidths=0.5)
     return plot
-
 
 
 ################### Feature_engineering#######################
@@ -99,6 +98,7 @@ def bmi_cat(df):
     df['bmi_cat'] = pd.cut(x=df['bmi'], bins=[18.5,25,30,35,40,99],
                          labels = ['Normal range', 'Over-weight', 'Obese (class 1)','Obese (class 2)', 'Obese (class 3)'])
     return df
+
 
 # blood pressure category (medium.com)
 def pressure_cat(df):
